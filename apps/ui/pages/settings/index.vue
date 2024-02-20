77a77f8602a3fe83ee3dea0b5f1cc149df12b8ae
yourmoln <yourmoln@outlook.com>
2024-02-21 02:58:37 +0800

    feat: 设计了设置界面的ui
    这段代码仅供展示，未能使用
    
    后续需根据配置文件生成page

--- apps/ui/pages/settings/index.vue
+++ apps/ui/pages/settings/index.vue
@@ -1,1 +1,32 @@
-<template>settings</template>
+<template>
+  <n-space class="m-1%" vertical>
+    <n-card :title="$text.settings()" size="small"></n-card>
+    <n-card :title="$text.header.general()" size="small">
+      <n-list hoverable clickable>
+        <n-list-item>
+          <n-thing :title="$text.switch_theme()">
+            {{ $text.switch_theme_caption() }}
+          </n-thing>
+        </n-list-item>
+      </n-list>
+    </n-card>
+    <n-card :title="$text.header.user()" size="small">
+      <n-list hoverable clickable>
+        <n-list-item>
+          <n-thing :title="$text.verify_mail()">
+            {{ $text.verify_mail_caption() }}
+          </n-thing>
+        </n-list-item>
+        <n-list-item>
+          <n-thing :title="$text.change_psw()">
+            {{ $text.change_psw_caption() }}
+          </n-thing>
+        </n-list-item>
+      </n-list>
+    </n-card>
+  </n-space>
+</template>
+
+<script setup lang="ts">
+const { $text } = useNuxtApp();
+</script>
