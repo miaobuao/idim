13e13876df603cda3898a3bc671b463126d65826
miaobuao <miaobuao@outlook.com>
2024-03-05 00:40:02 +0800

    fix: process.env

--- apps/web/server/trpc/utils/config.ts
+++ apps/web/server/trpc/utils/config.ts
@@ -1,9 +1,7 @@
-import { env } from 'node:process'
-
 export default {
-  IS_DEVELOPMENT: env.NODE_ENV === 'development',
-  LOG_LEVEL: env.LOG_LEVEL ?? 'info',
-  JWT_SECRET: env.JWT_SECRET!,
-  JWT_EXPIRES_IN: env.JWT_EXPIRES_IN ?? '7d',
-  JWT_ISSUER: env.JWT_ISSUER!,
+  IS_DEVELOPMENT: process.env.NODE_ENV === 'development',
+  LOG_LEVEL: process.env.LOG_LEVEL ?? 'info',
+  JWT_SECRET: process.env.JWT_SECRET!,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ?? '7d',
+  JWT_ISSUER: process.env.JWT_ISSUER!,
 }
